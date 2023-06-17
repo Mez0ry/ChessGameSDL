b87e3c44050fa778b74585d0aab8855f6aeb7804
Mez0ry <612315123@mail.ru>
2023-06-17 19:50:34 +0300

    Added getters

--- src/Board.hpp
+++ src/Board.hpp
@@ -1,9 +1,9 @@
 #ifndef __BOARD_HPP__
 #define __BOARD_HPP__
-#include <array>
 #include "Base.hpp"
-#include "Texture.hpp"
 #include "Player.hpp"
+#include "Texture.hpp"
+#include <array>
 
 #define BOARD_HEIGHT 8
 #define BOARD_WIDTH 8
@@ -12,29 +12,36 @@ class Board {
 public:
   Board(const Base::Ref<Renderer> renderer);
   ~Board();
-  
+
   void OnResize(const Base::Ref<Window> window);
   void Update(float dt);
   void Render();
   static void RenderEntity(const Base::Ref<Renderer> renderer,const Base::Ref<Entity> entity);
-public: //Functional
- void LoadPositionFromFen(const char *fen, std::vector<Player>& players);
 
-public: //Accessors
-  uint32_t GetArrayIndexAt(const Vec2& pos) const{
+  void LoadPositionFromFen(const char *fen, std::vector<Player> &players);
+
+public:
+  bool IsOnBoard(const Vec2 &pos) {
+    return (pos.y < m_BoardSize.y && pos.x < m_BoardSize.x && pos.y > -1 && pos.x > -1);
+  }
+
+  uint32_t GetArrayIndexAt(const Vec2 &pos) const {
     return (pos.y * BOARD_WIDTH + pos.x);
   }
 
-  ObjectSize GetOneSquareSize() const {return m_OneSquareSize;}
+  ObjectSize GetOneSquareSize() const { return m_OneSquareSize; }
+
+  const Vec2 &GetTopLeft() const { return m_BoardTopLeft; }
+
 private:
   std::array<uint8_t, BOARD_HEIGHT * BOARD_WIDTH> m_Board;
   Texture m_BlackSquareTexture;
   Texture m_WhiteSquareTexture;
-  const Vec2 m_BoardSize = {BOARD_WIDTH,BOARD_HEIGHT};
+  const Vec2 m_BoardSize = {BOARD_WIDTH, BOARD_HEIGHT};
 
 private:
   Base::Ref<Renderer> m_Renderer;
-  ObjectSize m_OneSquareSize = ObjectSize(128,128);
+  ObjectSize m_OneSquareSize = ObjectSize(128, 128);
   static Vec2 m_BoardTopLeft;
 };
 #endif //! __BOARD_HPP__
