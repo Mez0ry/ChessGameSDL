27a0c9bbe1f9af5be74dad905abb6d011c254f4f
Mez0ry <612315123@mail.ru>
2023-06-19 18:38:02 +0300

    Removed specialMove and promotionTo vars

--- src/MoveInfo.hpp
+++ src/MoveInfo.hpp
@@ -1,7 +1,8 @@
 #ifndef __MOVE_INFO_HPP__
 #define __MOVE_INFO_HPP__
 #include "Vector.hpp"
-#include "Piece.hpp"
+
+class Piece;
 
 #define SHORT_CASTLE_MASK 1 << 0
 #define LONG_CASTLE_MASK  1 << 1
@@ -10,13 +11,11 @@
 
 struct MoveInfo
 {
-    MoveInfo() : pieceToMove(nullptr),pieceToKill(nullptr), specialMove(0), promotionTo(Piece::PieceType::UNKNOWN) {}
+    MoveInfo() : pieceToMove(nullptr),pieceToKill(nullptr) {}
     Base::Ref<Piece> pieceToMove;
     Vec2 moveFrom;
     Vec2 moveTo;
-    uint8_t specialMove; // 0b - shortCastle, 1b - LongCastle, 2b - enpassant, 3b protomotion
-    Piece::PieceType promotionTo;
-    
+ 
     Vec2 killedPos;
     Base::Ref<Piece> pieceToKill;
 };
